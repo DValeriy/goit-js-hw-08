7cfd2763d51c176f3bcf7c80aa613f05f28236d7
DValeriy <valeriydm20@gmail.com>
2021-10-01 14:00:42 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import fnLodTrot from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const item = localStorage.getItem("videoplayer-current-time")
+
+let onPlay = function (data) { localStorage.setItem("videoplayer-current-time", `${data.seconds}`) };
+
+player.on('timeupdate', fnLodTrot(onPlay, 1000));
+
+let timeToStart = item ? item : "0";
+
+player.setCurrentTime(timeToStart)
+
+
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import fnLodTrot from "lodash.throttle";
+const formNode = document.querySelector(".feedback-form")
+const emailInputNode = document.querySelector(".feedback-form input")
+const messageInputNode = document.querySelector(".feedback-form textarea")
+let objToLS = {}
 
+const handleInput = e => {
+  const { email, message } = formNode.elements
+  objToLS = {
+    email:email.value,
+    message:message.value
+  }
+  localStorage.setItem("feedback-form-state",JSON.stringify(objToLS))
+}
+formNode.addEventListener("input",fnLodTrot(handleInput,500))
+
+const checkForm = ()=> {
+  let status = localStorage.getItem("feedback-form-state") 
+  if (status ) {
+    emailInputNode.value = JSON.parse(status).email;
+    messageInputNode.value = JSON.parse(status).message;
+    objToLS = {
+    email:emailInputNode.value,
+    message:messageInputNode.value
+  }
+  }
+}
+checkForm()
+
+const handleSubmit = e => {
+  e.preventDefault()
+  emailInputNode.value=''
+  messageInputNode.value=''
+  localStorage.clear()
+  console.log(objToLS)
+}
+formNode.addEventListener("submit", handleSubmit);
